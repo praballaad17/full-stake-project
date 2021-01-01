99dfb4653e83fa0db2b40154bd91c7af1f6bc71b
prabal laad <praballaad17@gmail.com>
2021-01-01 23:23:32 +0530

    initial author routes

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-// if (process.env.NODE_ENV !== 'production') {
-//     require('dotenv').parse()
-// }
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({path:'.env'});
+}
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}) )
 
-// const mongoose = require('mongoose')
-// mongoose.connect(process.env.DATABSE_URL)
-// const db = mongoose.connection
-// db.on('error', error => console.error(error))
-// db.once('open', () => console.log('Connected to mongoose'))
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorsRouter)
+
 app.listen(process.env.PORT || 3001)
